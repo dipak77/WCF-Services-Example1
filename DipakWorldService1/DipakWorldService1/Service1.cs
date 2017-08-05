03ebbe8ca34ca4f0a572d09c9eeed51bd30bdf69
dipak77 <haranedipak@gmail.com>
2017-08-05 17:50:26 +0530

    commit

---
+++ DipakWorldService1/DipakWorldService1/Service1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DipakWorldService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public int calculateDays(int day, int Month, int year)
+        {
+            DateTime dt = new DateTime(year, Month, day);
+            int datetodays = DateTime.Now.Subtract(dt).Days;
+            return datetodays;
+        }
+    }
+}
